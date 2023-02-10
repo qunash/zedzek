554ce7a61afc7b417364ed7ffafc59982c78a24d
qunash <anzoria@gmail.com>
2023-02-10 09:12:59 +0000

    ~fix icons

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -11,7 +11,7 @@ export function MainNav() {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="items-center space-x-2 flex">
-        <Icons.logo className="h-6 w-6" />
+        <Icons.logo className="h-6 w-6 fill-current" />
         <span className="font-bold inline-block">
           {siteConfig.name}
         </span>

df6a144e4cc1ac7e5c1cc07dac9e055ac308bf60
qunash <anzoria@gmail.com>
2023-03-28 12:35:27 +0300

    +opengraph image

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ export default function IndexPage() {
         <title>{siteConfig.name}</title>
         <meta name="description" content={siteConfig.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="https://zedzek.com/api/og"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">

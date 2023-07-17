6e5891fa310379c4caf76c86e3c6bb852d5cbe7a
qunash <anzoria@gmail.com>
2023-07-17 05:05:04 +0000

    ~UI, refactoring

--- components/translationPanel.tsx
+++ components/translationPanel.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react"
-
+import { Key, useState } from "react"
 import { Icons } from "./icons"
 import { buttonVariants } from "./ui/button"
 import { useTranslations } from "next-intl"
@@ -10,7 +9,7 @@ const IconButton = ({ icon, onClick, clickedIcon = null, isClicked = false }) =>
     className={buttonVariants({
       size: "lg",
       variant: "ghost",
-      className: "items-center justify-end self-starttext-slate-700 dark:text-slate-400 cursor-pointer",
+      className: "items-center justify-end self-starttext-slate-700 dark:text-slate-400 cursor-pointer"
     })}
     onClick={onClick}
   >
@@ -30,47 +29,49 @@ const EmptyState = () => (<div className="h-80 w-full rounded-r-lg p-4 md:h-96" 
 
 const TranslationItem = ({ translation }) => <div>{translation}</div>
 
-const TranslationPanel = (props: {
-  loading: boolean
-  translations: string[]
-  duration: number
-}) => {
+const TranslationPanel = ({ loading, translations, duration }) => {
   const t = useTranslations("Translator")
   const { locale } = useRouter()
 
-  const [copyClicked, setCopyClicked] = useState(false)
+  const [iconClicked, setIconClicked] = useState({ copy: false, thumbsUp: false, thumbsDown: false })
 
-  function onCopyClick(event: React.MouseEvent<HTMLDivElement>) {
+  function onIconClick(event: { currentTarget: { blur: () => void } }, type: string) {
     event.currentTarget.blur()
-    setCopyClicked(true)
-    navigator.clipboard.writeText(props.translations[0])
-    setTimeout(() => {
-      setCopyClicked(false)
-    }, 1500)
+
+    switch (type) {
+      case 'copy':
+        setIconClicked({ ...iconClicked, copy: true })
+        navigator.clipboard.writeText(translations[0])
+        setTimeout(() => setIconClicked({ ...iconClicked, copy: false }), 1500)
+        break
+      case 'thumbsUp':
+      case 'thumbsDown':
+        setIconClicked(prev => ({ ...prev, [type]: !prev[type], [type === 'thumbsUp' ? 'thumbsDown' : 'thumbsUp']: false }))
+        break
+    }
   }
 
-  if (props.loading) {
-    return <LoadingState />
-  }
-
-  if (props.translations.length == 0) {
-    return <EmptyState />
-  }
+  if (loading) return <LoadingState />
+  if (translations.length === 0) return <EmptyState />
 
   return (
     <div className="min-h-80 md:min-h-96 h-fit w-full rounded-r-lg p-4">
       <div className="flex h-full flex-col items-center justify-center pt-4">
         <div className="h-full w-full overflow-y-auto p-4">
-          <div className="text-xl">{props.translations[0]}</div>
+          <div className="text-xl">{translations[0]}</div>
           <div className="my-4 h-px w-full bg-gray-500" />
           <div className="flex w-full flex-row items-center justify-between">
             <IconButton
               icon={<Icons.thumbsUp className="h-4 w-4" />}
-              onClick={() => { }}
+              clickedIcon={<Icons.thumbsUp className="h-4 w-4 fill-current" />}
+              isClicked={iconClicked.thumbsUp}
+              onClick={(e) => onIconClick(e, 'thumbsUp')}
             />
             <IconButton
               icon={<Icons.thumbsDown className="h-4 w-4" />}
-              onClick={() => { }}
+              clickedIcon={<Icons.thumbsDown className="h-4 w-4 fill-current" />}
+              isClicked={iconClicked.thumbsDown}
+              onClick={(e) => onIconClick(e, 'thumbsDown')}
             />
             <IconButton
               icon={<Icons.edit className="h-4 w-4" />}
@@ -79,20 +80,20 @@ const TranslationPanel = (props: {
             <IconButton
               icon={<Icons.copy className="h-4 w-4" />}
               clickedIcon={<Icons.check className="h-4 w-4" />}
-              isClicked={copyClicked}
-              onClick={onCopyClick}
+              isClicked={iconClicked.copy}
+              onClick={(e) => onIconClick(e, 'copy')}
             />
           </div>
           <div className="pb-2 pt-4 text-xl text-gray-500">{t("alternatives", { locale })}:</div>
           <div className="flex flex-col gap-2">
-            {props.translations.slice(1).map((translation, index) => (
+            {translations.slice(1).map((translation, index: Key) => (
               <TranslationItem key={index} translation={translation} />
             ))}
           </div>
         </div>
         <div className="flex w-full flex-row items-center justify-between p-4">
           <div className="text-xs text-gray-500">
-            {Math.round(props.duration * 100) / 100}s
+            {Math.round(duration * 100) / 100}s
           </div>
         </div>
       </div>

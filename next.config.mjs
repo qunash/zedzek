f2d4d47db155cc5fa10ccbfe8212c6e84ef3b717
qunash <anzoria@gmail.com>
2023-08-06 13:34:05 +0000

    ~linting

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    fontLoaders: [
-      {
-        loader: "next/font/google",
-        options: { subsets: ["latin"] },
-      },
-    ],
-  },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
   i18n: {
     locales: ["en", "ru", "tr", "ar"],
     defaultLocale: "en",
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function IndexPage() {
         <meta property="og:image" content="https://zedzek.com/api/og" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10 justify-items-center">
+      <section className="container grid items-center justify-items-center gap-6 pb-8 pt-6 md:py-10">
         <h1 className="text-xl font-bold lg:text-2xl">
           {t('header', { locale })}
         </h1>

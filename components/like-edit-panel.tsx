3dfebcffa37cf7d3f51a0c0f64157f36fc53e44a
qunash <anzoria@gmail.com>
2023-08-06 13:01:17 +0000

    ~refactor action buttons

---
+++ components/like-edit-panel.tsx
@@ -0,0 +1,117 @@
+import { Database } from "@/types/database.types"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { AuthError, PostgrestError, User } from "@supabase/supabase-js"
+import { useTranslations } from "next-intl"
+import { useRouter } from "next/router"
+import { cloneElement, useEffect, useState } from "react"
+import { Icons } from "./icons"
+import { Button, buttonVariants } from "./ui/button"
+
+export default function LikeAndEdit({ text, translation }) {
+    const supabase = createClientComponentClient<Database>()
+    const [user, setUser] = useState<User | null | undefined>()
+    const [showSignIn, setShowSignIn] = useState(false)
+    const [isLiked, setIsLiked] = useState(false)
+    const { locale } = useRouter()
+    const t = useTranslations("Index")
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const supaSession = await supabase.auth.getSession()
+            if (supaSession?.data) {
+                setUser(supaSession.data.session?.user)
+            }
+        }
+
+        fetchUser()
+
+        const { data: listener } = supabase.auth.onAuthStateChange(
+            async (event, newSession) => {
+                if (event === 'SIGNED_OUT') {
+                    setUser(null)
+                } else if (event === 'SIGNED_IN' && newSession) {
+                    setUser(newSession.user)
+                }
+            }
+        )
+
+        return () => {
+            listener?.subscription.unsubscribe()
+        }
+    }, [supabase.auth])
+
+    const logError = (error) => console.error('Error:', error)
+
+    const handleSignIn = async () => {
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: { redirectTo: window.location.href }
+        })
+        if (error) logError(error)
+    }
+
+    const handleLike = async () => {
+        if (!user) {
+            setShowSignIn(true)
+            return
+        }
+
+        if (isLiked) {
+            const { error } = await supabase.from('translations')
+                .delete()
+                .match({ user_id: user?.id, text, translation })
+            setIsLiked(false)
+            if (error) logError(error)
+        } else {
+            const { error } = await supabase.from('translations').insert({
+                user_id: user?.id,
+                text,
+                translation,
+                is_user_translation: false,
+            })
+            setIsLiked(true)
+            if (error) logError(error)
+        }
+    }
+
+    const handleEdit = async () => {
+        if (!user) {
+            setShowSignIn(true)
+            return
+        }
+        console.log('edit')
+    }
+
+
+    const SignInDiv = (
+        <div className="flex w-full flex-row items-center justify-center rounded-r-lg p-4">
+            <Button onClick={handleSignIn}>
+                {t('sign_in', { locale })}
+            </Button>
+        </div>
+    )
+
+    if (showSignIn) return SignInDiv
+
+    const ButtonDiv = ({ icon, isFilled = false, onClick }) => {
+        const baseClass = buttonVariants({
+            size: "lg",
+            variant: "ghost",
+            className: "items-center justify-end self-start text-slate-700 dark:text-slate-400 cursor-pointer"
+        })
+
+        return (
+            <div className={baseClass} onClick={handleLike}>
+                {cloneElement(icon, { className: `h-4 w-4 ${isFilled ? "fill-current" : ""}` })}
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex w-full flex-row items-center justify-between">
+            <ButtonDiv icon={<Icons.thumbsUp />} isFilled={isLiked} onClick={handleLike} />
+            <ButtonDiv icon={<Icons.thumbsDown />} onClick={handleEdit} />
+            <ButtonDiv icon={<Icons.edit />} onClick={handleEdit} />
+        </div>
+    )
+}
--- components/translationPanel.tsx
+++ components/translationPanel.tsx
@@ -1,130 +1,54 @@
-import { useSession, signIn } from 'next-auth/react';
-import { useTranslations } from "next-intl"
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-import { Icons } from "./icons"
-import { Button, buttonVariants } from "./ui/button"
-
-const IconButton = ({ icon, onClick, clickedIcon = null, isClicked = false }: { icon: React.ReactNode, onClick: (e: any) => Promise<void>, clickedIcon?: React.ReactNode | null, isClicked?: boolean }) => (
-
-  <div
-    className={buttonVariants({
-      size: "lg",
-      variant: "ghost",
-      className: "items-center justify-end self-starttext-slate-700 dark:text-slate-400 cursor-pointer"
-    })}
-    onClick={onClick}
-  >
-    {isClicked && clickedIcon ? clickedIcon : icon}
-  </div>
-);
-
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Icons } from "./icons";
+import { buttonVariants } from "./ui/button";
+import LikeAndEdit from './like-edit-panel';
 
 const LoadingState = () => (
   <div className="flex h-80 w-full flex-col items-center justify-center rounded-r-lg p-4 md:h-96">
     <div className="h-5 w-5 animate-spin rounded-full border-y-2 border-gray-500" />
   </div>
-)
+);
 
-const EmptyState = () => (<div className="h-80 w-full rounded-r-lg p-4 md:h-96" />)
+const EmptyState = () => <div className="h-80 w-full rounded-r-lg p-4 md:h-96" />;
 
-const SignInDiv = () => {
-  const { locale } = useRouter()
-  const t = useTranslations("Index")
-  return (
-    <div className="flex w-full flex-row items-center justify-center rounded-r-lg p-4">
-      <Button onClick={() => signIn('google')}>
-        {t('sign_in', { locale })}
-      </Button>
-    </div>
-  )
-}
-
-const TranslationItem = ({ translation }) => <div>{translation}</div>
+const ErrorState = ({ error }) => (
+  <div className="flex h-80 w-full items-center justify-center rounded-r-lg p-4 md:h-96">
+    <div className='text-center text-xl'>{error.message}</div>
+  </div>
+);
 
 const TranslationPanel = ({ translationResponse, loading }) => {
-  const { data: session, status } = useSession()
-  const { locale } = useRouter()
-  const t = useTranslations("Translator")
+  const { locale } = useRouter();
+  const t = useTranslations("Translator");
 
   const emptyState = {
     text: '',
     translations: [],
     duration: 0,
-    iconClicked: { copy: false, upvote: false, downvote: false },
-    upvoted: false,
-    showSignIn: false
+    isCopyClicked: false,
   };
 
   const [state, setState] = useState(emptyState);
 
   useEffect(() => {
-    // console.log('Translation response', translationResponse);
     if (translationResponse && !(translationResponse instanceof Error)) {
-      setState({
-        ...translationResponse,
-        iconClicked: { copy: false, upvote: false, downvote: false },
-        upvoted: false,
-        showSignIn: false,
-      });
+      setState({ ...translationResponse, isCopyClicked: false });
     } else {
       setState(emptyState);
     }
-  }, [translationResponse])
+  }, [translationResponse]);
 
-  const onIconClick = async (event, type) => {
-    event.currentTarget.blur()
-    const updateState = newState => setState(prevState => ({ ...prevState, ...newState }))
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(state.translations[0]);
+    setState(prevState => ({ ...prevState, isCopyClicked: true }));
+    setTimeout(() => setState(prevState => ({ ...prevState, isCopyClicked: false })), 1500);
+  };
 
-    switch (type) {
-      case 'copy':
-        navigator.clipboard.writeText(state.translations[0])
-        updateState({ iconClicked: { ...state.iconClicked, copy: true } })
-        setTimeout(() => updateState({ iconClicked: { ...state.iconClicked, copy: false } }), 1500)
-        break
-      case 'upvote':
-        if (status === 'unauthenticated') {
-          updateState({ showSignIn: true });
-        } else {
-          const body = JSON.stringify({
-            removeUpvote: state.upvoted,
-            userId: session?.user.id,
-            text: state.text,
-            translation: state.translations[0],
-          })
-          try {
-            const response = await fetch('/api/supabase/handle-upvote', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body })
-            const data = await response.json()
-            // console.log('Translation updated', data);
-            updateState({
-              upvoted: data.upvoted,
-              iconClicked: { ...state.iconClicked, upvote: !state.iconClicked.upvote, downvote: false }
-            })
-          } catch (error) {
-            console.error('An error occurred while updating the translation:', error);
-          }
-        }
-        break
-      case 'downvote':
-      case 'edit':
-        if (status === 'unauthenticated') {
-          updateState({ showSignIn: true })
-        }
-        break
-    }
-  }
-
-  if (loading) return <LoadingState />
-
-  if (translationResponse instanceof Error) {
-    return (
-      <div className="flex h-80 w-full items-center justify-center rounded-r-lg p-4 md:h-96">
-        <div className='text-center text-xl'>{translationResponse.message}</div>
-      </div>
-    )
-  }
-
-  if (state.translations.length === 0) return <EmptyState />
+  if (loading) return <LoadingState />;
+  if (translationResponse instanceof Error) return <ErrorState error={translationResponse} />;
+  if (state.translations.length === 0) return <EmptyState />;
 
   return (
     <div className="min-h-80 md:min-h-96 h-fit w-full rounded-r-lg p-4">
@@ -133,34 +57,22 @@ const TranslationPanel = ({ translationResponse, loading }) => {
           <div className="text-xl">{state.translations[0]}</div>
           <div className="my-4 h-px w-full bg-gray-500" />
           <div className="flex w-full flex-row items-center justify-between">
-            <IconButton
-              icon={<Icons.thumbsUp className="h-4 w-4" />}
-              clickedIcon={<Icons.thumbsUp className="h-4 w-4 fill-current" />}
-              isClicked={state.iconClicked.upvote}
-              onClick={e => onIconClick(e, 'upvote')}
-            />
-            <IconButton
-              icon={<Icons.thumbsDown className="h-4 w-4" />}
-              clickedIcon={<Icons.thumbsDown className="h-4 w-4 fill-current" />}
-              isClicked={state.iconClicked.downvote}
-              onClick={e => onIconClick(e, 'downvote')}
-            />
-            <IconButton
-              icon={<Icons.edit className="h-4 w-4" />}
-              onClick={e => onIconClick(e, 'edit')}
-            />
-            <IconButton
-              icon={<Icons.copy className="h-4 w-4" />}
-              clickedIcon={<Icons.check className="h-4 w-4" />}
-              isClicked={state.iconClicked.copy}
-              onClick={e => onIconClick(e, 'copy')}
-            />
+            <LikeAndEdit text={state.text} translation={state.translations[0]} />
+            <div
+              className={buttonVariants({
+                size: "lg",
+                variant: "ghost",
+                className: "items-center justify-end self-starttext-slate-700 dark:text-slate-400 cursor-pointer"
+              })}
+              onClick={handleCopy}
+            >
+              {state.isCopyClicked ? <Icons.check className="h-4 w-4" /> : <Icons.copy className="h-4 w-4" />}
+            </div>
           </div>
-          {state.showSignIn && <SignInDiv />}
           <div className="pb-2 pt-4 text-xl text-gray-500">{t("alternatives", { locale })}:</div>
           <div className="flex flex-col gap-2">
             {state.translations.slice(1).map((translation, index) => (
-              <TranslationItem key={index} translation={translation} />
+              <div key={index}>{translation}</div>
             ))}
           </div>
         </div>
@@ -172,6 +84,6 @@ const TranslationPanel = ({ translationResponse, loading }) => {
       </div>
     </div>
   )
-}
+};
 
-export default TranslationPanel
+export default TranslationPanel;

6f17fdfb3b21d1543430b87ab28513495077e998
qunash <anzoria@gmail.com>
2023-08-12 17:14:19 +0000

    +translation panel

---
+++ components/translationPanel.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+// import { useTranslations } from "next-intl"
+
+import { Icons } from "./icons"
+// import LikeAndEdit from "./like-edit-panel"
+import { buttonVariants } from "./ui/button"
+import { TranslationResponse } from "@/types/translation-response"
+
+const LoadingState = () => (
+  <div className="flex h-80 w-full flex-col items-center justify-center rounded-r-lg p-4 md:h-96">
+    <div className="h-5 w-5 animate-spin rounded-full border-y-2 border-gray-500" />
+  </div>
+)
+
+const EmptyState = () => (
+  <div className="h-80 w-full rounded-r-lg p-4 md:h-96" />
+)
+
+const ErrorState = ({ error }) => (
+  <div className="flex h-80 w-full items-center justify-center rounded-r-lg p-4 md:h-96">
+    <div className="text-center text-xl">{error.message}</div>
+  </div>
+)
+
+const TranslationPanel = ({ translationResponse, loading } : { translationResponse?: TranslationResponse | null, loading: boolean }) => {
+//   const { locale } = useRouter()
+//   const t = useTranslations("Translator")
+
+  const emptyState = {
+    text: "",
+    translations: [],
+    duration: 0,
+    isCopyClicked: false,
+  }
+
+  const [state, setState] = useState(emptyState)
+
+  useEffect(() => {
+    if (translationResponse && !(translationResponse instanceof Error)) {
+      setState({ ...translationResponse, isCopyClicked: false })
+    } else {
+      setState(emptyState)
+    }
+  }, [translationResponse])
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(state.translations[0])
+    setState((prevState) => ({ ...prevState, isCopyClicked: true }))
+    setTimeout(
+      () => setState((prevState) => ({ ...prevState, isCopyClicked: false })),
+      1500
+    )
+  }
+
+  if (loading) return <LoadingState />
+  if (translationResponse instanceof Error)
+    return <ErrorState error={translationResponse} />
+  if (state.translations.length === 0) return <EmptyState />
+
+  return (
+    <div className="min-h-80 md:min-h-96 h-fit w-full rounded-r-lg p-4">
+      <div className="flex h-full flex-col items-center justify-center pt-4">
+        <div className="h-full w-full overflow-y-auto p-4">
+          <div className="text-xl">{state.translations[0]}</div>
+          <div className="my-4 h-px w-full bg-gray-500" />
+          <div className="flex w-full flex-row items-center justify-between">
+            {/* <LikeAndEdit
+              text={state.text}
+              translation={state.translations[0]}
+            /> */}
+            <div
+              className={buttonVariants({
+                size: "lg",
+                variant: "ghost",
+                className:
+                  "items-center justify-end self-starttext-slate-700 dark:text-slate-400 cursor-pointer",
+              })}
+              onClick={handleCopy}
+            >
+              {state.isCopyClicked ? (
+                <Icons.check className="h-4 w-4" />
+              ) : (
+                <Icons.copy className="h-4 w-4" />
+              )}
+            </div>
+          </div>
+          <div className="pb-2 pt-4 text-xl text-gray-500">
+            {/* {t("alternatives", { locale })}: */}
+            Alternatives:
+          </div>
+          <div className="flex flex-col gap-2">
+            {state.translations.slice(1).map((translation, index) => (
+              <div key={index}>{translation}</div>
+            ))}
+          </div>
+        </div>
+        <div className="flex w-full flex-row items-center justify-between p-4">
+          <div className="text-xs text-gray-500">
+            {Math.round(state.duration * 100) / 100}s
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TranslationPanel
--- components/translator.tsx
+++ components/translator.tsx
@@ -5,6 +5,10 @@ import '@/styles/globals.css'
 import { TranslationResponse } from '@/types/translation-response'
 import { useEffect, useRef, useState } from "react"
 import { StringParam, useQueryParam, withDefault } from 'use-query-params'
+import { Icons } from './icons'
+import { buttonVariants } from './ui/button'
+import Examples from './examples'
+import TranslationPanel from './translationPanel'
 
 export default function Translator() {
     const [textParam, setTextParam] = useQueryParam('text', withDefault(StringParam, ''))
@@ -47,21 +51,48 @@ export default function Translator() {
         focusOnTextArea()
     }
 
+    const onExampleClick = (example: string) => {
+        window.scrollTo(0, 0)
+        setText(example)
+        setTextParam(example, "replaceIn")
+    }
+
 
     if (translationResponse instanceof Error)
         return <p>{translationResponse.message}</p>
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <form>
-                <textarea className='w-full h-32 p-2 border-2 rounded-xl focus:outline-none'
-                    ref={textareaRef}
-                    onInput={onTextInput}
-                    value={text}
+        <div>
+            <div className="flex max-w-4xl flex-col items-start rounded-lg border border-gray-800 shadow-lg md:flex-row">
+                <div className="flex w-full rounded-l-lg border-gray-800 p-4 md:h-96">
+                    <textarea
+                        ref={textareaRef}
+                        className="h-full min-h-[150px] w-full resize-none bg-transparent p-2 text-xl focus:outline-none focus:ring-0"
+                        // placeholder={t("type_to_translate", { locale })}
+                        placeholder='Type to translate...'
+                        value={text}
+                        onInput={onTextInput}
+                    />
+                    <div
+                        className={buttonVariants({
+                            size: "sm",
+                            variant: "ghost",
+                            className: `items-center justify-end self-start p-4 text-slate-700 dark:text-slate-400 cursor-pointer ${text.length == 0 ? "hidden" : ""
+                                }`,
+                        })}
+                        onClick={onClearClick}
+                    >
+                        <Icons.close className="h-5 w-5 fill-current" />
+                    </div>
+                </div>
+                <div className="h-px w-full bg-gray-800 md:h-full md:w-px" />
+                <TranslationPanel
+                    translationResponse={translationResponse}
+                    loading={loading}
                 />
-            </form>
-            {loading ? <p>Loading...</p> : translationResponse && <p>{translationResponse.translations.toString()}</p>}
-        </div>
+            </div>
+            <Examples onExampleClick={onExampleClick} />
+        </div >
     )
 
     async function api_translate(inputText?: string) {

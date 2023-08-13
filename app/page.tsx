b64c0db599a669278733cbff8559da441941652c
qunash <anzoria@gmail.com>
2023-08-13 09:12:41 +0000

    +google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import Translator from "@/components/translator"
 import NextQueryParamAdapter from "@/lib/next-query-params-adapter"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 import { QueryParamProvider } from "use-query-params"
 
 export default function Home() {
   return (
     // <QueryParamProvider adapter={NextAdapterApp}>
     <QueryParamProvider adapter={NextQueryParamAdapter}>
+      <GoogleAnalytics trackPageViews />
       <section className="container grid justify-items-center gap-4">
         <h1 className="text-xl font-bold lg:text-2xl">
           {/* {t("header", { locale })} */}

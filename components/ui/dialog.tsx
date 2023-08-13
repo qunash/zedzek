2b8d1b1558a82b6ef36d321e47b7d11b9375142c
qunash <anzoria@gmail.com>
2023-08-13 17:48:50 +0000

    ~cleanup

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { X } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -114,11 +113,6 @@ const DialogDescription = React.forwardRef<
 DialogDescription.displayName = DialogPrimitive.Description.displayName
 
 export {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
-  DialogDescription,
+  Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger
 }
+

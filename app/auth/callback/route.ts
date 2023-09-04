acf8385eefc3840cc2f5ddc7f9f26eada74ad431
qunash <anzoria@gmail.com>
2023-09-04 18:47:10 +0400

    ~debug sign-in redirecting

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,16 +5,20 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url)
     const code = requestUrl.searchParams.get("code")
-    const originalUrl =
+    const redirectUrl =
         requestUrl.searchParams.get("redirect_to") || requestUrl.origin
 
-    requestUrl.searchParams.delete("redirect_to")
-    requestUrl.searchParams.delete("code")
+    console.log("~~~~~~~~~~~~~ requestUrl :", requestUrl)
+    console.log("~~~~~~~~~~~~~ code       :", code)
+    console.log("~~~~~~~~~~~~~ originalUrl:", redirectUrl)
+
+    // requestUrl.searchParams.delete("redirect_to")
+    // requestUrl.searchParams.delete("code")
 
     if (code) {
         const supabase = createRouteHandlerClient<Database>({ cookies })
         await supabase.auth.exchangeCodeForSession(code)
     }
 
-    return NextResponse.redirect(originalUrl)
+    return NextResponse.redirect(redirectUrl)
 }

3bd6abc8a2857210c87b00315b4a855a03feaa15
qunash <anzoria@gmail.com>
2023-09-05 14:38:18 +0400

    ~debugging

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,14 @@ import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
-export const getBaseUrl = () =>
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000"
+export const getBaseUrl = () => {
+    const env = process.env.NEXT_PUBLIC_VERCEL_ENV
+
+    if (env === "production") {
+        return process.env.NEXT_PUBLIC_SITE_URL // set manually by me to https://www.zedzek.com/ in Vercel dashboard
+    } else if (env === "preview" || env === "development") {
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    } else {
+        return "http://localhost:3000"
+    }
+}

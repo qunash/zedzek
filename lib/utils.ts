bc1fbb742fce3eff006e93203080083741dfc928
qunash <anzoria@gmail.com>
2023-09-05 14:45:15 +0400

    ~debugging sign-in redirect

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export const getBaseUrl = () => {
     const env = process.env.NEXT_PUBLIC_VERCEL_ENV
 
     if (env === "production") {
-        return process.env.NEXT_PUBLIC_SITE_URL // set manually by me to https://www.zedzek.com/ in Vercel dashboard
+        return process.env.NEXT_PUBLIC_SITE_URL // set manually by me to https://www.zedzek.com in Vercel dashboard
     } else if (env === "preview" || env === "development") {
         return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     } else {

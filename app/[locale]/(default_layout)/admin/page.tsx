8afd67a57a35454552dddcda8fbaff9ca0398e82
qunash <anzoria@gmail.com>
2024-02-29 17:29:53 +0300

    ~ ugly fix for type error

--- app/[locale]/(default_layout)/admin/page.tsx
+++ app/[locale]/(default_layout)/admin/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 import { Separator } from "@radix-ui/react-dropdown-menu"
-import { User, createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import {
+    createClientComponentClient,
+    User,
+} from "@supabase/auth-helpers-nextjs"
 
 import { Profile } from "@/types/supabase"
 import { Button } from "@/components/ui/button"
 import { UserAvatar } from "@/components/user-avatar"
-import { useRouter } from 'next/navigation'
 
 export default function AdminPage() {
     const supabase = createClientComponentClient<Database>()
@@ -16,11 +19,11 @@ export default function AdminPage() {
     const [profileLoaded, setProfileLoaded] = useState(false)
     const router = useRouter()
 
-    const [roleRequests, setRoleRequests] = useState<
-        (Database["public"]["Tables"]["role_access_requests"]["Row"] & {
-            profiles: Database["public"]["Tables"]["profiles"]["Row"]
-        })[]
-    >([])
+    type RoleRequestWithProfile = Database["public"]["Tables"]["role_access_requests"]["Row"] & {
+        profiles: Database["public"]["Tables"]["profiles"]["Row"] | null;
+    }
+    
+    const [roleRequests, setRoleRequests] = useState<RoleRequestWithProfile[]>([])
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -49,7 +52,7 @@ export default function AdminPage() {
     }, [supabase, supabase.auth])
 
     useEffect(() => {
-        console.log('user changed', user)
+        console.log("user changed", user)
         const fetchProfile = async (user_id: string) => {
             const { data } = await supabase
                 .from("profiles")
@@ -64,9 +67,9 @@ export default function AdminPage() {
     }, [supabase, user])
 
     useEffect(() => {
-        console.log('profile changed', profile)
+        console.log("profile changed", profile)
         if (profileLoaded && profile?.role !== "admin") {
-            console.log('role', profile?.role)
+            console.log("role", profile?.role)
             router.push("/")
         }
     }, [profile, router])
@@ -135,45 +138,49 @@ export default function AdminPage() {
         }
     }
 
-    return profile?.role === 'admin' && (
-        <div className="w-1/3">
-            <div className="space-y-0.5">
-                <h2 className="text-2xl font-bold tracking-tight">
-                    Role Access Requests
-                </h2>
-            </div>
-            <Separator className="my-6" />
-            <div className="flex flex-col gap-4">
-                {roleRequests.map((request) => (
-                    <div className="flex items-center justify-between space-x-4">
-                        <div className="flex w-fit gap-4">
-                            <UserAvatar profile={request.profiles} />
-                            <div className="flex-col">
-                                <p className="text-sm font-medium leading-none">
-                                    {request.profiles?.username}
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    {request.profiles?.role}
-                                </p>
+    return (
+        profile?.role === "admin" && (
+            <div className="w-1/3">
+                <div className="space-y-0.5">
+                    <h2 className="text-2xl font-bold tracking-tight">
+                        Role Access Requests
+                    </h2>
+                </div>
+                <Separator className="my-6" />
+                <div className="flex flex-col gap-4">
+                    {roleRequests.map((request) => (
+                        <div className="flex items-center justify-between space-x-4">
+                            <div className="flex w-fit gap-4">
+                                <UserAvatar profile={request.profiles!} />
+                                <div className="flex-col">
+                                    <p className="text-sm font-medium leading-none">
+                                        {request.profiles?.username}
+                                    </p>
+                                    <p className="text-sm text-muted-foreground">
+                                        {request.profiles?.role}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex w-fit gap-4">
+                                <Button
+                                    variant="secondary"
+                                    className="shrink-0"
+                                    onClick={() => handleDeny(request.user_id)}
+                                >
+                                    Deny
+                                </Button>
+                                <Button
+                                    onClick={() =>
+                                        handleApprove(request.user_id)
+                                    }
+                                >
+                                    Approve
+                                </Button>
                             </div>
                         </div>
-                        <div className="flex w-fit gap-4">
-                            <Button
-                                variant="secondary"
-                                className="shrink-0"
-                                onClick={() => handleDeny(request.user_id)}
-                            >
-                                Deny
-                            </Button>
-                            <Button
-                                onClick={() => handleApprove(request.user_id)}
-                            >
-                                Approve
-                            </Button>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        )
     )
 }

8a87952883c8dacee4a74ae32a981520ecc5a151
qunash <anzor.qunash@gmail.com>
2025-03-24 02:57:58 +0300

    ~fix: logo click refreshes page to clear translator state

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -9,10 +9,10 @@ export async function MainNav() {
 
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
+      <a href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6 fill-current" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
-      </Link>
+      </a>
 
       <Link href="/contribute" className="flex items-center self-center font-medium text-foreground/80 transition-colors hover:text-foreground sm:text-sm">
         <span className="inline-block">

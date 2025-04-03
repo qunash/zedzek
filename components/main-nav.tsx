c1686d6d9cda9e36f791e55331d9d77516088349
qunash <anzor.qunash@gmail.com>
2025-04-03 06:49:23 +0300

    ~ui: remove `contribute` from header

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,15 +10,15 @@ export async function MainNav() {
   return (
     <div className="flex gap-6 md:gap-10">
       <a href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6 fill-current" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <Icons.logo className="h-10 w-10 fill-current" />
+        <span className="inline-block font-bold text-xl">{siteConfig.name}</span>
       </a>
 
-      <Link href="/contribute" className="flex items-center self-center font-medium text-foreground/80 transition-colors hover:text-foreground sm:text-sm">
+      {/* <Link href="/contribute" className="flex items-center self-center font-medium text-foreground/80 transition-colors hover:text-foreground sm:text-sm">
         <span className="inline-block">
           {t("index.contribute")}
         </span>
-      </Link>
+      </Link> */}
     </div>
   )
 }

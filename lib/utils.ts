d618e606a24201fd502e49ea0b89fe4fd77c880c
qunash <anzor.qunash@gmail.com>
2025-11-02 16:43:05 +0200

    ~getBaseUrl() to support Netlify env vars

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,30 +4,17 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const getBaseUrl = () => {
-    if (typeof window !== "undefined" && window.location?.origin) {
-        console.log("1. Returning window.location.origin:", window.location.origin)
-        return window.location.origin
+
+    const context = process.env.CONTEXT
+
+    if (context === "production") {
+        return process.env.URL
     }
 
-    const netlifyUrl =
-        process.env.NEXT_PUBLIC_SITE_URL ||
-        process.env.SITE_URL ||
-        process.env.URL
-    if (netlifyUrl) {
-        console.log("process.env.NEXT_PUBLIC_SITE_URL:", process.env.NEXT_PUBLIC_SITE_URL)
-        console.log("process.env.SITE_URL:", process.env.SITE_URL)
-        console.log("process.env.URL:", process.env.URL)
-        console.log("2. Returning netlifyUrl:", netlifyUrl)
-        return netlifyUrl
-    }
-
-    const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-    if (vercelUrl) {
-        console.log("process.env.NEXT_PUBLIC_VERCEL_URL:", process.env.NEXT_PUBLIC_VERCEL_URL)
-        console.log("3. Returning vercelUrl:", vercelUrl)
-        return `https://${vercelUrl}`
-    }
-
-    console.log("4. Returning fallback: http://localhost:3000")
-    return "http://localhost:3000"
+    return (
+        process.env.DEPLOY_URL ??
+        process.env.DEPLOY_PRIME_URL ??
+        process.env.URL ??
+        "http://localhost:3000"
+    )
 }

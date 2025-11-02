ae89ee5e0828017b36d894f0982d94159f3194f4
qunash <anzor.qunash@gmail.com>
2025-11-02 16:47:35 +0200

    +logs for debugging

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,10 +7,17 @@ export const getBaseUrl = () => {
 
     const context = process.env.CONTEXT
 
+    console.log("context", context)
+
     if (context === "production") {
+        console.log("Redirecting to production URL, process.env.URL", process.env.URL)
         return process.env.URL
     }
 
+    console.log("Redirecting to deploy URL, process.env.DEPLOY_URL", process.env.DEPLOY_URL)
+    console.log("Redirecting to deploy prime URL, process.env.DEPLOY_PRIME_URL", process.env.DEPLOY_PRIME_URL)
+    console.log("Redirecting to URL, process.env.URL", process.env.URL)
+    console.log("Redirecting to fallback URL, http://localhost:3000")
     return (
         process.env.DEPLOY_URL ??
         process.env.DEPLOY_PRIME_URL ??

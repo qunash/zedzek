f78bfa6c6ec91317cb3d063baa1a8cee8d65dc33
qunash <anzor.qunash@gmail.com>
2025-11-02 19:00:21 +0200

    ~manually remove query params on redirect (Netlify-specific)

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,14 +5,17 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url)
     const code = requestUrl.searchParams.get("code")
-    const redirectUrlParam = requestUrl.searchParams.get("redirect_url") || requestUrl.origin
 
     if (code) {
         const supabase = createRouteHandlerClient<Database>({ cookies })
         await supabase.auth.exchangeCodeForSession(code)
     }
 
-    // Unlike Vercel, Netlify preserves query parameters on GET redirects, so we need to manually strip them
-    const { origin, pathname } = new URL(redirectUrlParam)
-    return NextResponse.redirect(`${origin}${pathname}`)
+    // Clean redirect URL to prevent Netlify from preserving query params
+    const redirectUrl = new URL(
+        requestUrl.searchParams.get("redirect_url") || requestUrl.origin
+    )
+    redirectUrl.search = ""
+
+    return NextResponse.redirect(redirectUrl.toString())
 }

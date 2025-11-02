ae095f2d89268fde75b42d5188be9b9f88d3ae79
qunash <anzor.qunash@gmail.com>
2025-11-02 17:03:13 +0200

    ~getBaseUrl() to support Netlify env vars

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,24 +4,25 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const getBaseUrl = () => {
+    if (typeof window !== "undefined") {
+        return window.location.origin
+    }
 
-    const context = process.env.CONTEXT
+    if (process.env.NEXT_PUBLIC_SITE_URL) {
+        return process.env.NEXT_PUBLIC_SITE_URL
+    }
 
-    console.log("context", context)
-
-    if (context === "production") {
-        console.log("Redirecting to production URL, process.env.URL", process.env.URL)
+    if (process.env.URL) {
         return process.env.URL
     }
 
-    console.log("Redirecting to deploy URL, process.env.DEPLOY_URL", process.env.DEPLOY_URL)
-    console.log("Redirecting to deploy prime URL, process.env.DEPLOY_PRIME_URL", process.env.DEPLOY_PRIME_URL)
-    console.log("Redirecting to URL, process.env.URL", process.env.URL)
-    console.log("Redirecting to fallback URL, http://localhost:3000")
-    return (
-        process.env.DEPLOY_URL ??
-        process.env.DEPLOY_PRIME_URL ??
-        process.env.URL ??
-        "http://localhost:3000"
-    )
+    if (process.env.DEPLOY_URL) {
+        return process.env.DEPLOY_URL
+    }
+
+    if (process.env.DEPLOY_PRIME_URL) {
+        return process.env.DEPLOY_PRIME_URL
+    }
+
+    return "http://localhost:3000"
 }
